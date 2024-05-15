896803ea41be2b2f28f87f4718609001de40c822
Corey Graham <142066414+Wintersgate@users.noreply.github.com>
2024-05-14 20:12:07 -0500

    Made basic operator functions, basic ui, and started css

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1, operator, num2) {
+    if (operator == '+') {
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiply(num1, num2) 
+    } else if (operator == '/') {
+        return divide(num1, num2)
+    }
+};
+
+console.log(operate(2, '-', 2));
