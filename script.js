48c2f538d7f14125a28091d1631bea0b50aa88b9
Corey Graham <142066414+Wintersgate@users.noreply.github.com>
2024-06-08 19:03:20 -0500

    Scaled back code, changed updateDisplay, combined all operator functions into a single function

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-const display = document.querySelector('#display');
-const calBtns = document.querySelector('#buttons');
-const clear = document.querySelector('#clear');
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let displayValue = '0';
 
-let num1;
-let num2;
-let operator;
-
-const array1 = [];
-
-calBtns.addEventListener('click', (event) => {
-    array1.push(event.target.value);
-    display.innerText = array1.join('');
-    console.log(array1);
-});
-
-function add(num1, num2) {
-    return num1 + num2;
+function updateDisplay() {
+    display = document.getElementById('display');
+    display.innerText = displayValue;
 }
+updateDisplay();
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function operate(num1, operator, num2) {
+function operate(firstNumber, operator, secondNumber) {
     if (operator == '+') {
-        return add(num1, num2);
+        return firstNumber + secondNumber;
     } else if (operator == '-') {
-        return subtract(num1, num2);
+        return firstNumber - secondNumber;
     } else if (operator == '*') {
-        return multiply(num1, num2); 
+        return firstNumber * secondNumber; 
     } else if (operator == '/') {
-        return divide(num1, num2);
+        return firstNumber / secondNumber;
     }
 };
-
-console.log(operate(2, '-', 2));
