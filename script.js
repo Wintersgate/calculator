959408d96fcb60240c4c568f197da8c097b9dec0
Corey Graham <142066414+Wintersgate@users.noreply.github.com>
2024-06-08 19:57:14 -0500

    Implemented event listener to update display with input, limited input to 9 characters

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ let firstNumber = null;
 let secondNumber = null;
 let operator = null;
 let displayValue = '0';
+const firstArray = [];
+const buttons = document.querySelectorAll('button');
+
+window.addEventListener('click', (event) => {
+    firstArray.push(event.target.value);
+    displayValue = firstArray.join('');
+    updateDisplay();
+})
 
 function updateDisplay() {
     display = document.getElementById('display');
     display.innerText = displayValue;
+    if (displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
 }
 updateDisplay();
 
+// function to calculate from two numbers and an operator
+// Add, subtract, multiply, divide
 function operate(firstNumber, operator, secondNumber) {
     if (operator == '+') {
         return firstNumber + secondNumber;
